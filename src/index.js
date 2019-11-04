b8ed7f48a931261612db5b3c9efb85a1e69ced15
Ernesto del Frade <ernerock6@gmail.com>
2019-11-03 21:10:33 -0500

    adding ie11 polifills

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+/* IE 11 Support */
+import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/stable';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App/';
