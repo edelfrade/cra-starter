d6c18f69b5ac2cf6c6db622f2594b4e1955dd769
Ernesto del Frade <ernerock6@gmail.com>
2019-11-04 00:38:15 -0500

    Feature/eslint (#4)
    * eslint
    
    * adding underdocre camel rule excemption

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,22 +1,22 @@
 import React, { lazy, Suspense } from 'react';
-import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
+import {
+  BrowserRouter as Router, Switch, Route, Redirect,
+} from 'react-router-dom';
 import '../../assets/styles/main.scss';
 
 const Home = lazy(() => import('../../pages/Homepage'));
 
-const App = () => {
-  return (
-    <Router>
-      <Suspense fallback={<div className="loader"></div>}>
-        <Switch>
-          <Route path="/">
-            <Home />
-          </Route>
-          <Redirect to="/" />
-        </Switch>
-      </Suspense>
-    </Router>
-  );
-}
+const App = () => (
+  <Router>
+    <Suspense fallback={<div className="loader" />}>
+      <Switch>
+        <Route path="/">
+          <Home />
+        </Route>
+        <Redirect to="/" />
+      </Switch>
+    </Suspense>
+  </Router>
+);
 
 export default App;
