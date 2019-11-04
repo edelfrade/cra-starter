7f973f709d7c8d639160d5d5774c4f3699e06a74
Ernesto del Frade <ernerock6@gmail.com>
2019-11-03 19:34:49 -0500

    removing unnesesary require

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-require('dotenv').config();
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
