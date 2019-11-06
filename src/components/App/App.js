31745323afda572a17f795ef8603b78d754ec17c
Ernesto del Frade <ernerock6@gmail.com>
2019-11-06 00:52:49 -0500

    replacing react-snapshot with react-snap

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,23 +1,21 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import {
   BrowserRouter as Router, Switch, Route, Redirect,
 } from 'react-router-dom';
 import '../../assets/styles/main.scss';
 
-// TODO: Currently react-snapshot does not supporl the Suspend API, looking for solutions
-// const Home = lazy(() => import('../../pages/Homepage'));
-import Home from '../../pages/Homepage';
+const Home = lazy(() => import('../../pages/Homepage'));
 
 const App = () => (
   <Router>
-    {/* <Suspense fallback={<div className="loader" />}> */}
-    <Switch>
-      <Route path="/">
-        <Home />
-      </Route>
-      <Redirect to="/" />
-    </Switch>
-    {/* </Suspense>  */}
+    <Suspense fallback={<div className="loader" />}>
+      <Switch>
+        <Route path="/">
+          <Home />
+        </Route>
+        <Redirect to="/" />
+      </Switch>
+    </Suspense> 
   </Router>
 );
 
--- src/index.js
+++ src/index.js
@@ -3,12 +3,19 @@ import 'react-app-polyfill/ie11';
 import 'react-app-polyfill/stable';
 
 import React from 'react';
-import { render } from 'react-snapshot';
+import { hydrate, render } from 'react-dom';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
 
-render(<App />, document.getElementById('root'));
+const rootElement = document.getElementById('root');
+if (rootElement.hasChildNodes()) {
+  hydrate(<App />, rootElement);
+} else {
+  render(<App />, rootElement);
+}
+
+// render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
